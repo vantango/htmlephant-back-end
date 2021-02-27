5bdfb492a27a9e3558798cb55553c50cfc17f8fd
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-02-27 12:04:20 -0800

    created route to seed NPC collection with fake data for demo

--- controllers/npcController.js
+++ controllers/npcController.js
@@ -25,10 +25,40 @@ const seedData = [
     }
 ]
 
-// Seed route for NPC characters
+// Some fake NPCs to throw them off the scent
+const fakeData = [
+    {
+        name: "Luther",
+        dialogue: ["Like sands through the hourglass, my hopes for your success dwindle by the second.", "As Sun Tzu said in his 'Art of War,'...*sigh* Never mind. ", "The details of your incompetence do not interest me."]
+    },
+    {
+        name: "Eli",
+        dialogue: ["We don't get too many of your kind around these parts.", "Did I ever tell you I was struck by lightning seven times?", "Oh, hey, it's you! Didn't think I'd be seeing you again. Most that go in there don't come out again."]
+    },
+    {
+        name: "The Great Agatha",
+        dialogue: ["Come closer, young one. I do not harm those who have not yet displeased me.", "Fool! Your arrogance will ensure you rot along with all those who came before you!", "*horrible screaming*"]
+    },
+    {
+        name: "Denise",
+        dialogue: ["...", "*appears silently*", "SHHH. It's almost time."]
+    }
+]
+
+
+// Seed route for NPC real characters
 router.get("/seednpc", (req, res) => {
     db.Npc.create(seedData).then(result => {
         res.send(`Congratulations! You have created: ${JSON.stringify(seedData, null, 2)}`)
+    }).catch(err => {
+        err ? res.status(500).send(err.message) : res.status(200).send("Success!")
+    });
+});
+
+// Seed route for fake NPC characters
+router.get("/faker", (req, res) => {
+    db.Npc.create(fakeData).then(result => {
+        res.send(`Congratulations! You have created: ${JSON.stringify(fakeData, null, 2)}`)
     }).catch(err => {
         err ? res.status(500).send(err.message) : res.status(200).send("Success!")
     });

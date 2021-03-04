7a08fa81e1187feec94e6b5eb6da0507b6896af5
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-03-03 18:43:22 -0800

    added deployed url to cors middleware

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-    origin:["http://localhost:3000"]
+    origin:["https://wizards-and-whiteboards.herokuapp.com/"]
 }));
 
 // Connect to mongoose database
